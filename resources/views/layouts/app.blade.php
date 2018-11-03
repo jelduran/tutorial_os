891960405b1ec16411c6861ceb05a06e10107270
John Elvis Duran <jelduran@misena.edu.co>
2018-11-03 03:35:24 +0000

    Add script to initialize material css framework in layouts/app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,6 +92,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/ripples.min.js"></script>
+    <script>
+        $.material.init();
+    </script>
     <script src="/js/app.js"></script>
 </body>
 </html>
