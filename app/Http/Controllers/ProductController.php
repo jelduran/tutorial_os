eb5091ae2c5b9d84e4fd0b08918807d6f6a0241f
John Elvis Duran <jelduran@misena.edu.co>
2018-11-22 07:18:34 +0000

    Behavior for show method in ProductController added.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -66,6 +66,9 @@ class ProductController extends Controller
     public function show($id)
     {
         //
+        $product = Product::find($id);
+        
+        return view('products.show', ['product' => $product]);
     }
 
     /**
