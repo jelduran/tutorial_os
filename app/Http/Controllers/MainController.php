58a3c46732126db1402dd39b63c17c797e72e3b8
John Elvis Duran <jelduran@misena.edu.co>
2018-12-04 15:53:58 +0000

    MainController edited.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     public function home(){
-        return view('main.home', ["name" => "John Elvis"]);
+        
+        return view('main.home');
     }
 }
