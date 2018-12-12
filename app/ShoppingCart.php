e44e03a24b0a6406164343ce65f0ad1c9fb1d677
John Elvis Duran <jelduran@misena.edu.co>
2018-12-12 22:56:38 +0000

    Model ShoppingCart edited

--- app/ShoppingCart.php
+++ app/ShoppingCart.php
@@ -10,12 +10,30 @@ class ShoppingCart extends Model
     
     protected $fillable = ['status'];
     
+    public function approve(){
+        $this->updateCustomIdAndStatus();
+    }
+    
+    public function generateCustomId(){
+        return md5("$this->id $this->updated_at");
+    }
+    
+    public function updateCustomIdAndStatus(){
+        $this->status = "approved";
+        $this->custom_id = $this->generateCustomId();
+        $this->save();
+    }
+    
     public function in_shopping_carts(){
         return $this->hasMany('App\InShoppingCart');
     }
     
     public function products(){
         return $this->belongsToMany('App\Product','in_shopping_carts');
+    }
+    
+    public function order(){
+        return $this->hasOne('App\Order')->first();
     }
     
     public function productsSize(){
