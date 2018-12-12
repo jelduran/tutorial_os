6e7485c445192d3c4524a072b34e2ff8b895ef72
John Elvis Duran <jelduran@misena.edu.co>
2018-12-12 05:29:48 +0000

    New method added to Product model.

--- app/Product.php
+++ app/Product.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
-    
+    public function paypalItem(){
+        return \PaypalPayment::item()
+                                ->setName($this->title)
+                                ->setDescription($this->description)
+                                ->setCurrency('USD')
+                                ->setQuantity(1)
+                                ->setPrice($this->pricing / 100);
+    }
 }
