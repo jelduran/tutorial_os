cdefc59b486acb1859f7717b4b555563575f5923
John Elvis Duran <jelduran@misena.edu.co>
2018-12-12 23:03:31 +0000

    Controller for model Order created

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Order;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $orders = \App\Order::latest()->get();
+        
+        return view('orders.index', ['orders' => $orders]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+}
