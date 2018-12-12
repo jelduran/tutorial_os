e542d5c8a5a7a3e8611efb6294ae69fc754bcd6c
John Elvis Duran <jelduran@misena.edu.co>
2018-12-12 04:11:47 +0000

    Controller for InShoppingCart model implemented

---
+++ app/Http/Controllers/InShoppingCartController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ShoppingCart;
+use App\InShoppingCart;
+
+class InShoppingCartController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $shopping_cart_id = \Session::get('shopping_cart_id');
+        
+        $shopping_cart = ShoppingCart::findOrCreateBySession($shopping_cart_id);
+        
+        $response = InShoppingCart::create([
+            "shopping_cart_id" => $shopping_cart->id,
+            "product_id" => $request->product_id,
+        ]);
+        
+        if ($response) {
+            return redirect('/carrito');
+        }else{
+            return back();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
