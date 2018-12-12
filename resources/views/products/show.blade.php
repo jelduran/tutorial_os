937c3e858b81be2a99ad5073a1b0f0eb883fe410
John Elvis Duran <jelduran@misena.edu.co>
2018-12-12 04:05:32 +0000

    View products.show edited, in_shopping_carts.form partial included.

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -24,7 +24,7 @@
                         {{ $product->description }}
                     </p>
                     <p>
-                        <a href="" class="btn btn-success">Agregar al carrito</a>
+                        @include('in_shopping_carts.form', ['product' => $product])
                     </p>
                 </div>
             </div>
