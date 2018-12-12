47b385cac6579b96672fc3ef31e1b2727e69ceb4
John Elvis Duran <jelduran@misena.edu.co>
2018-12-12 05:25:05 +0000

    Controller for ShoppingCart model implemented.

---
+++ app/Http/Controllers/ShoppingCartController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ShoppingCart;
+use App\PayPal;
+
+class ShoppingCartController extends Controller
+{
+    //
+    public function index(){
+        $shopping_cart_id = \Session::get('shopping_cart_id');
+        
+        $shopping_cart = ShoppingCart::findOrCreateBySession($shopping_cart_id);
+        
+        $paypal = new PayPal($shopping_cart);
+        
+        $payment = $paypal->generate();
+        
+        return redirect($payment->getApprovalLink());
+        
+        /*$products = $shopping_cart->products()->get();
+        
+        $total = $shopping_cart->total();
+        
+        return view('shopping_carts.index', ["products" => $products, "total" => $total]);*/
+    }
+}
