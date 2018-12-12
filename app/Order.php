5dcca3a74a517bdbfb60719c93d3bb59aa9cb9c0
John Elvis Duran <jelduran@misena.edu.co>
2018-12-12 23:02:47 +0000

    Model Order created

---
+++ app/Order.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'recipient_name',
+        'line1',
+        'line2',
+        'city',
+        'country_code',
+        'state',
+        'postal_code',
+        'email',
+        'shopping_cart_id',
+        'status',
+        'total',
+        'guide_number'
+        ];
+        
+    public function scopeLatest($query){
+        return $query->orderId()->monthly();
+    }
+    
+    public function scopeOrderId($query){
+        return $query->orderBy('id', 'desc');
+    }
+    
+    public function scopeMonthly($query){
+        return $query->whereMonth('created_at', '=', date('m'));
+    }
+        
+    public function address(){
+        return "$this->line1 $this->line2";
+    }
+    
+    public static function createFromPayPalResponse($response, $shopping_cart){
+        
+        $payer = $response->payer;
+        
+        $orderData = (array) $payer->payer_info->shipping_address;
+        $orderData = $orderData[key($orderData)];
+        $orderData['email'] = $payer->payer_info->email;
+        $orderData['total'] = $shopping_cart->total();
+        $orderData['shopping_cart_id'] = $shopping_cart->id;
+        
+        return Order::create($orderData);
+        
+    }
+}
